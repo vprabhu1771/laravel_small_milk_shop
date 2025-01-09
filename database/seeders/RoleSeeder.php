ad029ab2b23e19508d6c1eddc0ff06af5e514a38
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-10 00:12:59 +0530

    Create RoleSeeder.php
    php artisan make:seed RoleSeeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            ['name' => 'Admin'],
+            ['name' => 'Customer'],
+            ['name' => 'Delivery Agent']            
+        ];
+
+        foreach ($roles as $row) 
+        {
+            Role::create($row);
+        }
+    }
+}
