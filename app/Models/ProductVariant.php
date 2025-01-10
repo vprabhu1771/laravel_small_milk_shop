9268a8c19a7fd79cc8f8a5858baf29718fb13f42
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-10 19:08:38 +0530

    ProductVariant Model and Migration
    php artisan make:model ProductVariant -m

---
+++ app/Models/ProductVariant.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVariant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'qty',
+        'unit_price'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
---
+++ database/migrations/2025_01_10_133253_create_product_variants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variants', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('product_id');
+            
+            $table->string('qty'); // e.g., "1L", "500ML", "250ML"
+            
+            $table->decimal('unit_price', 8, 2); // Price for the specific quantity
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variants');
+    }
+};
