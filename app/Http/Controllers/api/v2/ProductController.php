5e750d879c1c9532970c6da852f8e6e6b91090de
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-11 18:42:07 +0530

    ProductController Transform the products logic added

--- app/Http/Controllers/api/v2/ProductController.php
+++ app/Http/Controllers/api/v2/ProductController.php
@@ -19,10 +19,27 @@ class ProductController extends Controller
         // Fetch all products with their variants
         $products = Product::with('variants')->get();
 
+        // Transform the products
+        $transformedProducts = $products->map(function ($product) {
+            return [
+                'id' => $product->id,
+                'name' => $product->name,
+                'image_path' => $product->GetImagePath(),
+                'variants' => $product->variants->map(function ($variant) {
+                    return [
+                        'product_id' => $variant->product_id,
+                        'id' => $variant->id,
+                        'qty' => $variant->qty,
+                        'unit_price' => $variant->unit_price,
+                    ];
+                }),
+            ];
+        });
+
         // Return as JSON response
         return response()->json([
             // 'success' => true,
-            'data' => $products
+            'data' => $transformedProducts
         ]);
     }
 }
